cbe2f6978acc976a8bed5beb31700ff8302657f5
moyv <2020302131171@whu.edu.cn>
2024-06-03 18:28:07 +0800

    finished

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// setTimeout(()=> {
+//     const slides = document.querySelector('.dot')
+//     console.log(slides);
+// }, 2000)
+
+const dots = document.querySelectorAll('.dot')
+console.log(dots);
